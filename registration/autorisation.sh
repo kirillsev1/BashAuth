c9cdeec0f7c987bf50976f9074b32d0d4aed6783
kirillsev1 <kirillprih47@gmail.com>
2022-10-22 13:42:52 +0300

    2110

--- registration/autorisation.sh
+++ registration/autorisation.sh
@@ -1,52 +1,71 @@
 #!/bin/bash
-for ((j=0;j < 3; j++))
-do
-    echo "Попытка №$(($j + 1))"
-    echo "Введите логин для авторизации"
-    read name
-    echo "Введите пароль"
-    read passw
-    i=1
-    flag=True
-    indname=0
-    while read line
-    do 
-        if [[ $(($i % 2)) == 1 ]]
-        then
-            if [[ $name == $line ]]
-            then
-                indname=$i
-                flag=False
-            fi
-        fi
-        i=$(($i + 1))
-    done < log.txt
-    ansflag=False
-    if [[ $flag == False ]]
-    then
+photo_flag=True
+if ! [[ -f "log.txt" ]]
+then
+    echo "Еще никто не регистрировался"
+else
+    for ((j=0;j < 3; j++))
+    do
+        echo "Попытка №$(($j + 1))"
+        echo "Введите логин для авторизации"
+        read name
+        echo "Введите пароль"
+        read -s passw
+        echo $passw | md5sum > timefile.txt
+        read passw < timefile.txt
         i=1
-        while read line
-        do
-            if [[ $(($i % 2)) == 0 ]]
-            then
-                if [[ $i == $(($indname + 1)) ]]
+        flag=True
+        indname=0
+            while read line
+            do
+                if [[ $(($i % 2)) == 1 ]]
                 then
-                    if [[ $passw == $line ]]
+                    if [[ $name == $line ]]
                     then
-                        ansflag=True
+                        indname=$i
+                        flag=False
                     fi
                 fi
-            fi
-            i=$(($i + 1))
-        done < log.txt
-        if [[ $ansflag == True ]]
+                i=$(($i + 1))
+            done < log.txt
+        
+        ansflag=False
+        if [[ $flag == False ]]
         then
-            echo "$name, добро пажаловать в систему"
-            break
+            i=1
+            while read line
+            do
+                if [[ $(($i % 2)) == 0 ]]
+                then
+                    if [[ $i == $(($indname + 1)) ]]
+                    then
+                        if [[ $passw  == $line ]]
+                        then
+                            ansflag=True
+                        fi
+                    fi
+                fi
+                i=$(($i + 1))
+            done < log.txt
+            if [[ $ansflag == True ]]
+            then
+                echo "$name, добро пажаловать в систему"
+                photo_flag=False
+                break
+            else
+                echo "Неверный пароль"
+            fi
         else
-            echo "Неверный пароль"
+            echo "Неверный логин"
         fi
-    else
-        echo "Неверный логин"
+    done
+    rm timefile.txt
+    if [[ $j == 3 ]]
+    then
+        if [[ ! -d photo ]]
+        then
+            mkdir photo
+        fi
+        fswebcam -r 640x480 --jpeg 85 -D 1 ./photo/`date +%d-%m-%y_%H-%M-%S`_$HOSTNAME.jpg
     fi
-done
+fi
--- registration/registration.sh
+++ registration/registration.sh
@@ -1,39 +1,53 @@
 #!/bin/bash
 infint=True
+while read line
+do
+    echo $line
+done < rules.txt
+
 while [[ $infint == True ]]
 do 
-    while read line
-    do
-        echo $line
-    done < rules.txt
     echo "Введите логин для регистрации"
     read name
+    if [[ $name == *" "* ]]
+    then
+        echo "Запись содержит пробелы"
+        continue
+    fi
     i=1
     flag=True
-    while read line
-    do 
-        if [[ $(($i % 2)) == 1 ]]
-        then
-            if [[ $name == $line ]]
+    if [[ -f "log.txt" ]]
+    then
+        while read line
+        do 
+            if [[ $(($i % 2)) == 1 ]]
             then
-                flag=False
+                if [[ $name == $line ]]
+                then
+                    flag=False
+                fi
             fi
-        fi
-        i=$(($i + 1))
-    done < log.txt
+            i=$(($i + 1))
+        done < log.txt
+    fi
     if [[ $flag == False ]]
     then
-        echo "Такой логин уже существует" \n
+        echo "Такой логин уже существует" 
     else
         echo "Придумайте пароль"
-        read passw
+        read -s passw
+        if [[ $passw == *" "* ]]
+        then
+            echo "Запись содержит пробелы"
+            continue
+        fi
         if [[ $passw == "" ]]
         then
-            echo "Пароль не может быть пустым" \n
+            echo "Пароль не может быть пустым" 
         else
             echo "Вы зарегистрированы"
             echo "$name" >> log.txt
-            echo "$passw" >> log.txt
+            echo $passw | md5sum >> log.txt
             infint=False
         fi
     fi
